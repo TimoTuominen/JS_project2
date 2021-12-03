5e4f6c483b8e5705e762fa22b2f1ef1f7bfc3193
Timo <timotuominen111@hotmail.com>
2021-12-03 02:20:18 +0200

    1

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let nappi1 = document.getElementById("nappi1");
+let teksti = document.getElementById("teksti");
 
 const getLanguage = fetch("https://www.finnkino.fi/xml/TheatreAreas/")
   .then((response) => response.text())
   .then((data) => {
     const parser = new DOMParser();
-    const xml = parser.parseFromString(data, "text/xml");
-    let tulos = xml.getElementsByTagName("ID");
-    console.log(tulos[0]);
+    const serializer = new XMLSerializer();
+
+    const xml = parser.parseFromString(data, "application/xml");
+
+    let tulos = xml.getElementsByTagName("ID").item(0);
+
+    const xmlStr = serializer.serializeToString(tulos);
+    console.log(xmlStr);
+    teksti.innerHTML = xmlStr;
+    //let tulosString = xml.toXMLString();
+    //teksti.innerHTML = tulosString;
+    //teksti.innerHTML =
+    //  tulos[0].getElementsByTagName("ID")[0].childNodes[0].nodeValue;
+    //teksti.innerHTML = tulosString;
   })
   .catch(console.error);
+
+//teksti.innerHTML = tulos[0];
 //console.log(getLanguage);
 
 //const request = fetch("https://www.finnkino.fi/xml/TheatreAreas/");
