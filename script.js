caff4658699bb2a80f702da0dffebaee8bd7cd7d
Timo <timotuominen111@hotmail.com>
2021-12-10 01:41:11 +0200

    1

--- script.js
+++ script.js
@@ -53,6 +53,14 @@ let elokuvahaku = function () {
           Search
         </button></td> `
           );
+
+          let haeTietokannasta = document.getElementById("nappi" + i);
+          haeTietokannasta.addEventListener("click", function () {
+            var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+            myWindow.document.write(
+              "<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>"
+            );
+          });
         }
       }
     })
