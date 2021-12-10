b893ea4198324fdb13ca3ef9a03652d71e6335b5
Timo <timotuominen111@hotmail.com>
2021-12-10 16:48:53 +0200

    1

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
-//let nappi1 = document.getElementById("nappi1");
-let taulukko = document.getElementById("taulukko");
+let taulukko = document.getElementById("taulukko"); // Muuttujien alustus ja elementtien haku
 let haeLeffat = document.getElementById("haeLeffat");
 let teatteriValinta = document.getElementById("teatteritValinta");
 let ylataulukko = document.getElementById("ylataulukko");
-//let valittuTeatteri = teatteriValinta.options[teatteriValinta.selectedIndex].value;
-let valintaTeksti = teatteriValinta.options[teatteriValinta.selectedIndex].text;
+let hakukentta = document.getElementById("hakukentta");
+let haeOMDBLeffat = document.getElementById("haeOMDBLeffat");
+let valintaTeksti = teatteriValinta.options[teatteriValinta.selectedIndex].text; // haetaan kentässä oleva arvo
 
 let elokuvahaku = function () {
-  fetch("https://www.finnkino.fi/xml/Schedule/")
-    .then((response) => response.text())
+  // Tehdään funktio tietojen noutoa varten
+  fetch("https://www.finnkino.fi/xml/Schedule/") // Tehdään haku Fetchiä käyttäen
+    .then((response) => response.text()) // Käsitellään ensimmäinen lupaus then käskyllä
     .then((data) => {
-      const parser = new DOMParser();
-      const serializer = new XMLSerializer();
+      // käsitellään seuraava lupaus ja saatu data
+      const parser = new DOMParser(); // Luodaan parseri XML tiedon käsittelyyn
+      const serializer = new XMLSerializer(); // Luodaan Serializer XML:n muuttamiseksi tekstimuotoon
 
-      const xml = parser.parseFromString(data, "application/xml");
-      let pituus = xml.getElementsByTagName("Theatre").length;
-      console.log(pituus);
+      const xml = parser.parseFromString(data, "application/xml"); // Tehdään saadusta datasta XML objekti
+      let pituus = xml.getElementsByTagName("Theatre").length; // Pituus For looppia varten
 
       for (let i = 0; i < pituus; i++) {
-        let tulos = xml.getElementsByTagName("Theatre").item(i);
-        const xmlStr = serializer.serializeToString(tulos);
+        // Käydään tulokset läpi For loopilla
+        let tulos = xml.getElementsByTagName("Theatre").item(i); // Otetaan elementit järjestyksessä käsittelyyn
+        const xmlStr = serializer.serializeToString(tulos); // Muutetaan objekti string muotoon
         let valittuTeatteri =
-          teatteriValinta.options[teatteriValinta.selectedIndex].value;
+          teatteriValinta.options[teatteriValinta.selectedIndex].value; // Haeataan select boxin arvo
 
         if (xmlStr == "<Theatre>" + valittuTeatteri + "</Theatre>") {
-          const elokuvanNimi = xml.getElementsByTagName("Title").item(i);
-          const elokuvanNimiStr = serializer.serializeToString(elokuvanNimi);
+          // tarkistetaan onko kyseessä halutun teatterin näytös
+          const elokuvanNimi = xml.getElementsByTagName("Title").item(i); // Haetaan elokuvan nimi
+          const elokuvanNimiStr = serializer.serializeToString(elokuvanNimi); // Muutetaan nimi String muotoon
           const elokuvanNimiClean = elokuvanNimiStr.replace(
+            // Poistetaan Stringistä tagit
             /<\/?[^>]+(>|$)/g,
             ""
           );
 
-          const kuvalinkki = xml
+          const kuvalinkki = xml // Toistetaan yllä oleva kuvalinkille
             .getElementsByTagName("EventSmallImagePortrait")
             .item(i);
           const kuvalinkkiStr = serializer.serializeToString(kuvalinkki);
           const kuvalinkkiClean = kuvalinkkiStr.replace(/<\/?[^>]+(>|$)/g, "");
 
-          const esitysaika = xml.getElementsByTagName("dttmShowStart").item(i);
+          const esitysaika = xml.getElementsByTagName("dttmShowStart").item(i); // Toistetaan ylläoleva esitysajalle
           const esitysaikaStr = serializer.serializeToString(esitysaika);
           const esitysaikaClean = esitysaikaStr.replace(/<\/?[^>]+(>|$)/g, "");
           const esitysaikaErikseen = esitysaikaClean.split("T");
           const esitysaikaKellonaika = esitysaikaErikseen[1].slice(0, 5);
 
-          const sali = xml.getElementsByTagName("TheatreAuditorium").item(i);
+          const sali = xml.getElementsByTagName("TheatreAuditorium").item(i); // Toistetaan ylläoleva salille
           const saliStr = serializer.serializeToString(sali);
           const saliClean = saliStr.replace(/<\/?[^>]+(>|$)/g, "");
 
           taulukko.insertAdjacentHTML(
+            // lisätään haetut tiedot HTML documenttiin
             "beforeend",
             `<tr><td  width="200"><img src="${kuvalinkkiClean}" alt="${kuvalinkkiStr}"> </td> <td><h4 id=elokuva${i}> ${elokuvanNimiClean} </h4><br>Esitysaika: ${esitysaikaErikseen[0]} 
           Klo:  ${esitysaikaKellonaika} <br> Esityspaikka: ${saliClean} </td></tr> <td><button class="btn btn-success" type="submit" id="nappi${i}">
@@ -55,27 +60,44 @@ let elokuvahaku = function () {
         </button></td> `
           );
 
-          let haeTietokannasta = document.getElementById("nappi" + i);
+          let haeTietokannasta = document.getElementById("nappi" + i); //haetaan painonapin ID muuttujaan
           haeTietokannasta.addEventListener("click", function () {
+            // Lisätään painonappiin toiminnaliisuutta painettaesssa
             let omdbHaku = function () {
               fetch(
                 `http://www.omdbapi.com/?apikey=bf626253&t=${elokuvanNimiClean}`
-              )
-                .then((response) => response.json())
-                .then((data) => haeOMDB(data));
+              ) //tehdään funktio joka hakee OMDP apia käyttäen tiedot
+                .then((response) => response.json()) // käsitellään ensimmäinen lupaus JSON:iksi
+                .then((data) => lisaaOMDB(data)); // Käsitellään toinen lupaus, kutsutaan lisäysfunktiota
             };
 
-            omdbHaku();
+            omdbHaku(); // Ajetaan funktio
           });
         }
       }
     })
-    .catch(console.error);
+    .catch(console.error); // Välitetään mahdolliset virheet konsoliin
 };
 
-function haeOMDB(data) {
-  ylataulukko.innerHTML = "";
+haeOMDBLeffat.addEventListener("click", function () {
+  //Lisätään kuuntelija hakukentän napille
+  ylataulukko.innerHTML = ""; // Tyhjennetään tulostus alue
+  let hakuteksti = hakukentta.value; // Haetaan hakukentän teksti
+  let omdbHaku2 = function () {
+    fetch(`http://www.omdbapi.com/?apikey=bf626253&t=${hakuteksti}`) //tehdään haku funktio OMDB tietoa varten
+      .then((response) => response.json())
+      .then((data) => lisaaOMDB(data)); // käsitellään kummatkin lupaukset
+  };
+
+  omdbHaku2(); // Kutsutaan haku funktiota
+  event.preventDefault(); // Estetään sivun päivittyminen
+});
+
+function lisaaOMDB(data) {
+  // Funktion OMDB tietojen lisäämiseen verkkosivulle
+  ylataulukko.innerHTML = ""; // Tyhjennetään taulukko
   ylataulukko.insertAdjacentHTML(
+    // Lisätään halutut tiedot
     "beforeend",
     `<tr><td>${data.Title}, Julkaistu: ${data.Released}</td><td text-align: left> Näyttelijät: ${data.Actors}</td></tr>
     <tr><td>Juoni: ${data.Plot} </td></tr> `
@@ -83,7 +105,8 @@ function haeOMDB(data) {
 }
 
 haeLeffat.addEventListener("click", function () {
-  taulukko.innerHTML = "";
-  elokuvahaku();
-  event.preventDefault();
+  // Funktio elokuvien hakuun paikkakunnittain
+  taulukko.innerHTML = ""; // Tyhjennetään taulukko
+  elokuvahaku(); // Kutsutaan tiedon haku funktiota
+  event.preventDefault(); // Estetään sivun päivittyminen
 });
